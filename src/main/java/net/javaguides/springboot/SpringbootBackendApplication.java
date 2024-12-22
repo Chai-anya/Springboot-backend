5ef99a98a9ba386722f05a2002cc906a7ac4f490
Krishna Chaitanya <v.krishnachaitanya1199@gmail.com>
2024-12-22 15:57:09 +0800

    Update SpringbootBackendApplication.java

--- src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -2,7 +2,7 @@ package net.javaguides.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// this is for checking webhook
 @SpringBootApplication
 public class SpringbootBackendApplication {
 
